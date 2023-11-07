19b0a8512cb42e1371a011afa904b3beb2d2e4d6
Steven Lu <singvision1729@gmail.com>
2023-11-07 11:24:35 +0800

    1. AModelChatGPT has been updated to be compatible with the latest version of openai.

--- llm/AModelChatGPT.py
+++ llm/AModelChatGPT.py
@@ -6,22 +6,20 @@ from common.AConfig import config
 
 class AModelChatGPT():
     def __init__(self, modelName: str):
-        openai.api_key = config.openaiGPTKey
         self.tokenizer = None
         self.modelName = modelName
+        self.client = openai.OpenAI(api_key = config.openaiGPTKey)
         return
     
     def Generate(self, prompt: list[dict[str,str]], proc: callable, endchecker: callable, temperature: float = 0.2) -> str:
         proc(channel='ASSISTANT', txt='', action='open')
         currentPosition = 0
         text = ""
-        for chunk in openai.ChatCompletion.create(model=self.modelName,
-                                                  messages=prompt,
-                                                  temperature=temperature,
-                                                  stream=True):
-            content = chunk["choices"][0].get("delta", {}).get("content")
-            if content is not None:
-                text += content
+        for chunk in self.client.chat.completions.create(model=self.modelName,
+                                                         messages=prompt,
+                                                         temperature=temperature,
+                                                         stream=True):
+            text += (chunk.choices[0].delta.content or "")
 
             if endchecker(text):
                 break
