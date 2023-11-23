9a3041533f28b90ac9ee7b5b5bd6315c15bbae99
Steven Lu <singvision1729@gmail.com>
2023-11-23 11:21:41 +0800

    1. scripter updated.

--- modules/AScripter.py
+++ modules/AScripter.py
@@ -7,44 +7,39 @@ from modules.AScrollablePage import AScrollablePage
 
 class AScripter():
     def __init__(self):
-        self.pageBash = AScrollablePage({"SCROLLUP": "SCROLLUPBASH<!||!>"})
-        self.pagePy = AScrollablePage({"SCROLLUP": "SCROLLUPPY<!||!>"})
+        self.pages = {"bash": AScrollablePage({"SCROLLUP": "SCROLLUPBASH<!||!>"}),
+                      "py": AScrollablePage({"SCROLLUP": "SCROLLUPPY<!||!>"})}
         return
     
     def ModuleInfo(self):
         return {"NAME": "scripter", "ACTIONS": {"BASH": "RunBash(code:str)->str", "SCROLLUPBASH": "ScrollUpBash()->str", "PYTHON": "RunPython(code:str)->str", "SCROLLUPPY": "ScrollUpPy()->str"}}
     
-    def RunBash(self, code: str) -> str:
+    def Run(self, session: str, cmd: list[str], timeout: int = None) -> str:
         try:
-            res = subprocess.check_output(code, shell=True, executable="/bin/bash", universal_newlines=True, timeout=60)
-        except subprocess.CalledProcessError as e:
-            res = str(e)
-        except subprocess.TimeoutExpired as e:
-            res = str(e)
-        finally:
-            self.pageBash.LoadPage(res, "BOTTOM")
-            return self.pageBash()
-
-    def ScrollUpBash(self) -> str:
-        self.pageBash.ScrollUp()
-        return self.pageBash()
-    
-    def RunPython(self, code: str) -> str:
-        try:
-            with tempfile.NamedTemporaryFile(mode='w', delete=True) as temp:
-                temp.write(code)
-                temp.flush()
-                result = subprocess.run(['python3', temp.name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-                res = result.stdout
+            result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=timeout)
+            res = result.stdout
         except Exception as e:
             res = f"Exception: {str(e)}\n"
         finally:
-            self.pagePy.LoadPage(res, "BOTTOM")
-            return self.pagePy()
+            self.pages[session].LoadPage(res, "BOTTOM")
+            return self.pages[session]()
+    
+    def RunBash(self, code: str) -> str:
+        return self.Run('bash', ["bash", "-c", code], timeout=60)
+    
+    def ScrollUpBash(self) -> str:
+        self.pages["bash"].ScrollUp()
+        return self.pages['bash']()
+    
+    def RunPython(self, code: str) -> str:
+        with tempfile.NamedTemporaryFile(mode='w', delete=True) as temp:
+            temp.write(code)
+            temp.flush()
+            return self.Run('py', ['python3', temp.name])
     
     def ScrollUpPy(self) -> str:
-        self.pagePy.ScrollUp()
-        return self.pagePy()
+        self.pages['py'].ScrollUp()
+        return self.pages['py']()
 
 
 if __name__ == '__main__':
