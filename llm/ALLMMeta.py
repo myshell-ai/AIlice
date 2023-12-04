15198ab480bf31fa6e255c536f5ac50a6a7a90eb
Steven Lu <singvision1729@gmail.com>
2023-12-04 14:47:45 +0800

    1. new LLMs. OpenHermes-2.5-Mistral-7B works pretty well.

--- llm/ALLMMeta.py
+++ llm/ALLMMeta.py
@@ -10,6 +10,8 @@ ALLMMeta={"hf:meta-llama/Llama-2-13b-chat-hf": {"formatter": AFormatterLLAMA2, "
           "hf:Xwin-LM/Xwin-LM-13B-V0.1": {"formatter": AFormatterVicuna, "contextWindow": 4096},
           "hf:mistralai/Mistral-7B-Instruct-v0.1": {"formatter": AFormatterLLAMA2, "contextWindow": 8192},
           "hf:Open-Orca/Mistral-7B-OpenOrca": {"formatter": AFormatterChatML, "contextWindow": 8192},
+          "hf:teknium/OpenHermes-2.5-Mistral-7B": {"formatter": AFormatterChatML, "contextWindow": 8192},
+          "hf:Intel/neural-chat-7b-v3-1": {"formatter": AFormatterSimple, "contextWindow": 8192},
           "hf:amazon/MistralLite": {"formatter": AFormatterAMAZON, "contextWindow": 16384},
           "hf:HuggingFaceH4/zephyr-7b-beta": {"formatter": AFormatterZephyr, "contextWindow": 8192},
           "hf:THUDM/agentlm-13b": {"formatter": AFormatterLLAMA2, "contextWindow": 4096}}
