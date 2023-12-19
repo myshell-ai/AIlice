71e89c7f907847a8fbb21ef4067efbca253ffa72
Steven Lu <singvision1729@gmail.com>
2023-12-19 11:36:48 +0800

    1. scripter executes scripts in a non-blocking manner.

--- modules/AScripter.py
+++ modules/AScripter.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import time
 import tempfile
 
 from common.lightRPC import makeServer
@@ -8,8 +10,8 @@ from modules.AScrollablePage import AScrollablePage
 class AScripter():
     def __init__(self, incontainer = False):
         self.incontainer = incontainer
-        self.pages = {"bash": AScrollablePage({"SCROLLUP": "SCROLLUPBASH"}),
-                      "py": AScrollablePage({"SCROLLUP": "SCROLLUPPY"})}
+        self.sessions = {"bash": {"proc": None, "pages": AScrollablePage({"SCROLLUP": "SCROLLUPBASH"})},
+                         "py": {"proc": None, "pages": AScrollablePage({"SCROLLUP": "SCROLLUPPY"})}}
         return
     
     def ModuleInfo(self):
@@ -18,32 +20,85 @@ class AScripter():
                                                 "PYTHON": {"sig": "RunPython(code:str)->str", "prompt": "Execute python code. Please note that you need to copy the complete code here, and you must not use references."},
                                                 "SCROLLUPPY": {"sig": "ScrollUpPy()->str", "prompt": "Scroll up the results."}}}
     
-    def Run(self, session: str, cmd: list[str], timeout: int = None) -> str:
-        try:
-            result = subprocess.run(cmd, env={"A_IN_CONTAINER": "1" if self.incontainer else "0"}, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, timeout=timeout)
-            res = result.stdout
-        except Exception as e:
-            res = f"Exception: {str(e)}\n"
-        finally:
-            self.pages[session].LoadPage(res, "BOTTOM")
-            return self.pages[session]()
+    def RunCMD(self, session: str, cmd: list[str], timeout: int = 30):
+        self.sessions[session]['proc'] = subprocess.Popen(cmd, env={"A_IN_CONTAINER": "1" if self.incontainer else "0"}, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1, universal_newlines=True)
+        os.set_blocking(self.sessions[session]['proc'].stdout.fileno(), False)
+        self.Wait(process=self.sessions[session]['proc'], timeout=timeout)
+        return
+    
+    def Wait(self, process, timeout):
+        t0 = time.time()
+        while time.time() < (t0 + timeout):
+            if process.poll() is not None:
+                return
+            time.sleep(0.5)
+    
+    def CheckOutput(self, session: str) -> tuple[str,bool]:
+        process = self.sessions[session]['proc']
+        output = ''
+        completed = False
+        if process.poll() is not None:
+            remainingOutput = process.stdout.read()
+            if remainingOutput:
+                output += remainingOutput
+            completed = True
+        else:
+            while True:
+                line = process.stdout.readline()
+                if line:
+                    output += line
+                else:
+                    break
+        return output, completed
     
     def RunBash(self, code: str) -> str:
-        return self.Run('bash', ["bash", "-c", code], timeout=60)
+        res = ""
+        if "" != code.strip():
+            try:
+                self.RunCMD('bash', ["bash", "-c", code])
+            except Exception as e:
+                res += f"Exception: {str(e)}\n"
+        
+        try:
+            output, completed = self.CheckOutput(session='bash')
+            res += output
+            res += "\nThe bash script takes longer to complete. You can pass an empty string to the BASH function to get new output." if not completed else "\nExecution completed."
+        except Exception as e:
+            res += f"Exception when check the output of bash execution: {str(e)}"
+            print(res)
+        finally:
+            self.sessions['bash']['pages'].LoadPage(res, "BOTTOM")
+            return self.sessions['bash']['pages']()
     
     def ScrollUpBash(self) -> str:
-        self.pages["bash"].ScrollUp()
-        return self.pages['bash']()
+        self.sessions['bash']['pages'].ScrollUp()
+        return self.sessions['bash']['pages']()
     
     def RunPython(self, code: str) -> str:
-        with tempfile.NamedTemporaryFile(mode='w', delete=True) as temp:
-            temp.write(code)
-            temp.flush()
-            return self.Run('py', ['python3', temp.name])
+        res = ""
+        if "" != code.strip():
+            with tempfile.NamedTemporaryFile(mode='w', delete=True) as temp:
+                temp.write(code)
+                temp.flush()
+                try:
+                    self.RunCMD('py', ['python3', '-u', temp.name])
+                except Exception as e:
+                    res += f"Exception: {str(e)}\n"
+        
+        try:
+            output, completed = self.CheckOutput(session='py')
+            res += output
+            res += "\nThe python script takes longer to complete. You can pass an empty string to the PYTHON function to get new output." if not completed else "\nExecution completed."
+        except Exception as e:
+            res += f"Exception when check the output of python execution: {str(e)}"
+            print(res)
+        finally:
+            self.sessions['py']['pages'].LoadPage(res, "BOTTOM")
+            return self.sessions['py']['pages']()
     
     def ScrollUpPy(self) -> str:
-        self.pages['py'].ScrollUp()
-        return self.pages['py']()
+        self.sessions['py']['pages'].ScrollUp()
+        return self.sessions['py']['pages']()
 
 
 if __name__ == '__main__':
