bcf9967a0b28dd47ed753e0aab0039003114a130
Steven Lu <singvision1729@gmail.com>
2023-12-24 15:07:00 +0800

    1. persistent storage for vecdb.

--- modules/AStorageChroma.py
+++ modules/AStorageChroma.py
@@ -6,20 +6,32 @@ from common.lightRPC import makeServer
 class AStorageChromaDB():
     def __init__(self):
         self.chroma_client = chromadb.Client()
-        self.collections = dict()
         return
 
     def ModuleInfo(self):
         return {"NAME": "storage", "ACTIONS": {}}
     
+    def Open(self, directory: str) -> str:
+        try:
+            if "" == directory.strip():
+                self.chroma_client = chromadb.Client()
+                return "chroma database has been switched to a non-persistent version."
+            else:
+                self.chroma_client = chromadb.PersistentClient(directory)
+                return f"chroma database under {directory} is opened."
+        except Exception as e:
+            print(f"Open() EXCEPTION. e: {str(e)}")
+            return f"Open() EXCEPTION. e: {str(e)}"
+    
+    def Reset(self) -> str:
+        self.chroma_client.reset()
+        return "chroma database reseted."
+    
     def Store(self, collection: str, txt: str) -> bool:
         try:
             print("collection: ", collection,". store: ", txt)
-            if not (collection in self.collections):
-                self.collections[collection] = self.chroma_client.create_collection(name=collection)
-
-            self.collections[collection].add(documents = [txt],
-                                            ids = [str(uuid.uuid4())])
+            self.chroma_client.get_or_create_collection(name=collection).add(documents = [txt],
+                                                                             ids = [str(uuid.uuid4())])
         except Exception as e:
             print("store() EXCEPTION: ", e)
             return False
@@ -27,7 +39,7 @@ class AStorageChromaDB():
     
     def Query(self, collection: str, clue: str, num_results:int=1):# -> list(tuple[str,float]):
         try:
-            res = self.collections[collection].query(query_texts=[clue],n_results=num_results)
+            res = self.chroma_client.get_collection(name=collection).query(query_texts=[clue],n_results=num_results)
             ret = None
             if (0 < len(res['documents'])):
                 ret = [(txt, dist) for txt, dist in zip(res['documents'][0], res['distances'][0])]
@@ -40,4 +52,4 @@ class AStorageChromaDB():
 
 
 storage = AStorageChromaDB()
-makeServer(storage, "ipc:///tmp/AIliceStorage.ipc", ["ModuleInfo", "Store", "Query"]).Run()
+makeServer(storage, "ipc:///tmp/AIliceStorage.ipc", ["ModuleInfo", "Open", "Reset", "Store", "Query"]).Run()
