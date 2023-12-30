440a183f71d204cd23104d7b48821662d79d0efe
Steven Lu <singvision1729@gmail.com>
2023-12-30 17:37:47 +0800

    1. Use appdirs to determine the location of config.json.

--- ailice/common/AConfig.py
+++ ailice/common/AConfig.py
@@ -1,5 +1,6 @@
 import os
 import json
+import appdirs
 from termcolor import colored
 
 class AConfig():
@@ -24,7 +25,14 @@ class AConfig():
         return
 
     def Initialize(self, needOpenaiGPTKey = False):
-        oldDict = self.Load("config.json")
+        configFile = appdirs.user_config_dir("ailice", "Steven Lu")
+        try:
+            os.makedirs(configFile)
+        except OSError as e:
+            pass
+        configFile += "/config.json"
+        
+        oldDict = self.Load(configFile)
         needUpdate = (set(oldDict.keys()) != set(self.__dict__))
         self.__dict__ = {k: oldDict[k] if k in oldDict else v for k,v in self.__dict__.items()}
         
@@ -36,18 +44,19 @@ class AConfig():
             if self.openaiGPTKey is None:
                 key = input(colored("Your openai chatgpt key (press Enter if not): ", "green"))
                 self.openaiGPTKey = key if 1 < len(key) else None
-            self.Store("config.json")
+            self.Store(configFile)
+            print(f"config.json is located at {configFile}.")
             print(colored("********************** End of Initialization *****************************", "yellow"))
         return
 
-    def Load(self, configFile: str = "config.json") -> dict:
+    def Load(self, configFile: str) -> dict:
         if os.path.exists(configFile):
             with open(configFile, "r") as f:
                 return json.load(f)
         else:
             return dict()
     
-    def Store(self, configFile: str = "config.json"):
+    def Store(self, configFile: str):
         with open(configFile, "w") as f:
             json.dump(self.__dict__, f, indent=2)
         return
