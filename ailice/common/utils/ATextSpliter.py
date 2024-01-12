2dba4af2960f814f3782cae00bb8a87ecfbcfe1b
Steven Lu <singvision1729@gmail.com>
2024-01-12 21:16:13 +0800

    1. article-digest reimplemented.

--- ailice/common/utils/ATextSpliter.py
+++ ailice/common/utils/ATextSpliter.py
@@ -3,3 +3,8 @@ import re
 def sentences_split(paragraph):
     for sent in re.findall(u'[^?。；，\.\?\;\,]+[?。；，\.\?\;\,]?', paragraph, flags=re.U):
         yield sent
+
+def paragraph_generator(text):
+    paragraphs = [p.strip() for p in text.split('\n\n') if p.strip()]
+    for paragraph in paragraphs:
+        yield paragraph
--- ailice/prompts/APromptArticleDigest.py
+++ ailice/prompts/APromptArticleDigest.py
@@ -1,5 +1,8 @@
 from importlib.resources import read_text
+from ailice.common.AConfig import config
+from ailice.common.utils.ATextSpliter import paragraph_generator
 from ailice.prompts.ARegex import GenerateRE4FunctionCalling
+from ailice.prompts.ATools import ConstructOptPrompt
 
 class APromptArticleDigest():
     PROMPT_NAME = "article-digest"
@@ -7,43 +10,45 @@ class APromptArticleDigest():
     def __init__(self, processor, storage, collection, conversations, formatter, outputCB = None):
         self.processor = processor
         self.storage = storage
-        self.collection = collection
+        self.collection = f"{collection}_{self.processor.name}_article"
         self.conversations = conversations
         self.formatter = formatter
         self.outputCB = outputCB
         self.prompt0 = read_text('ailice.prompts', 'prompt_article_digest.txt')
-        self.PATTERNS = {"BROWSE": [{"re": GenerateRE4FunctionCalling("BROWSE<!|url: str|!> -> str", faultTolerance = True), "isEntry": True}],
+        self.PATTERNS = {"READ": [{"re": GenerateRE4FunctionCalling("READ<!|url: str|!> -> str", faultTolerance = True), "isEntry": True}],
                          "SCROLLDOWN": [{"re": GenerateRE4FunctionCalling("SCROLLDOWN<!||!> -> str", faultTolerance = True), "isEntry": True}],
                          "SEARCHDOWN": [{"re": GenerateRE4FunctionCalling("SEARCHDOWN<!|keyword: str|!> -> str", faultTolerance = True), "isEntry": True}],
                          "SEARCHUP": [{"re": GenerateRE4FunctionCalling("SEARCHUP<!|keyword: str|!> -> str", faultTolerance = True), "isEntry": True}],
-                         "Output": [{"re": r"REPORT:(?P<txt>.*?)NOTEBOOK:", "isEntry": True}],
+                         "RETRIEVE": [{"re": GenerateRE4FunctionCalling("RETRIEVE<!|keywords: str|!> -> str", faultTolerance = True), "isEntry": True}],
                          "RESPOND": [{"re": GenerateRE4FunctionCalling("RESPOND<!|message: str|!> -> None", faultTolerance = True), "isEntry": True}]}
-        self.ACTIONS = {"Output": {"func": self.Output}}
+        self.ACTIONS = {"READ": {"func": self.Read},
+                        "RETRIEVE": {"func": self.Recall}}
         return
     
     def Reset(self):
         return
 
-    def Output(self, txt: str):
-        txt = txt.strip()
-        self.storage.Store(self.collection, txt)
-        self.outputCB(f"OUTPUT_{self.processor.name}", txt)
-        return
+    def Read(self, url: str) -> str:
+        ret = self.processor.modules['browser']['module'].Browse(url)
+        fulltxt = self.processor.modules['browser']['module'].GetFullText(url)
+        for txt in paragraph_generator(fulltxt):
+            self.storage.Store(self.collection, txt)
+        return ret
     
-    def Recall(self, key: str):
-        ret = self.storage.Query(self.collection, key, num_results=4)
-        for r in ret:
-            if (key not in r[0]) and (r[0] not in key):
-                return r[0]
-        return "None."
+    def Recall(self, keywords: str) -> str:
+        ret = self.storage.Query(self.collection, keywords, num_results=1)
+        if 0 != len(ret):
+            return ret[0][0]
+        else:
+            return "None."
     
     def GetPatterns(self):
         return self.PATTERNS
     
     def GetActions(self):
         return self.ACTIONS
-
-    def BuildPrompt(self):
+    
+    def ParameterizedBuildPrompt(self, n: int):
         context = self.conversations.GetConversations(frm = -1)[0]['msg']
         prompt = f"""
 {self.prompt0}
@@ -54,5 +59,11 @@ RELEVANT INFORMATION: {self.Recall(context).strip()}
 
 """
         #print(prompt)
-        return self.formatter(prompt0 = prompt, conversations = self.conversations.GetConversations(frm = -2))
+        ret = self.formatter(prompt0 = prompt, conversations = self.conversations.GetConversations(frm = -n))
+        return ret, self.formatter.Len(ret)
     
+    def BuildPrompt(self):
+        prompt, n = ConstructOptPrompt(self.ParameterizedBuildPrompt, low=1, high=len(self.conversations), maxLen=int(self.processor.llm.contextWindow * config.contextWindowRatio))
+        if prompt is None:
+            prompt = self.ParameterizedBuildPrompt(1)
+        return prompt
