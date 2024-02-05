65d08681485430ce17e13213bd3840a60a9bd1e4
Steven Lu <singvision1729@gmail.com>
2024-02-05 21:01:12 +0800

    1. __str__ for AImage.

--- ailice/common/ADataType.py
+++ ailice/common/ADataType.py
@@ -6,6 +6,9 @@ class AImage():
         self.data = data
         return
     
+    def __str__(self) -> str:
+        return f"AImage object in {self.format} format."
+    
     def ToJson(self):
         return {'format': self.format, 'data': base64.b64encode(self.data).decode('utf-8')}
     
