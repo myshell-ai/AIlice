abf15e5f0386ff0060eef0a735f1783c006f370e
Steven Lu <singvision1729@gmail.com>
2024-02-19 11:38:35 +0800

    1. AScrollablePage improved.

--- ailice/modules/AScrollablePage.py
+++ ailice/modules/AScrollablePage.py
@@ -9,16 +9,16 @@ class AScrollablePage():
         return
     
     def ConstructPrompt(self) -> str:
-        ret = "This is a page of the results. To browse more results, you can use the following functions.\n"
+        ret = "To avoid excessive consumption of context space due to lengthy content, we have paginated the entire content. This is just one page, to browse more content, please use the following function(s) for page navigation.\n"
         funcs = []
         if ('SCROLLDOWN' in self.functions) and (self.currentIdx + STEP < len(self.txt)):
             funcs.append(f"#scroll down the page: \n{self.functions['SCROLLDOWN']}<!||!>\n")
         if ('SCROLLUP' in self.functions) and (self.currentIdx > 0):
             funcs.append(f"#scroll up the page: \n{self.functions['SCROLLUP']}<!||!>\n")
         if ('SEARCHDOWN' in self.functions) and (self.currentIdx + STEP < len(self.txt)):
-            funcs.append(f"#search content downward from the current location by exact match: \n{self.functions['SEARCHDOWN']}<!|query: str|!>\n")
+            funcs.append(f"#search the content downward and jumps the page to the next matching point(Just like the F3 key normally does): \n{self.functions['SEARCHDOWN']}<!|query: str|!>\n")
         if ('SEARCHUP' in self.functions) and (self.currentIdx > 0):
-            funcs.append(f"#search content upward from the current location by exact match: \n{self.functions['SEARCHUP']}<!|query: str|!>\n")
+            funcs.append(f"#search the content upward and jumps the page to the next matching point: \n{self.functions['SEARCHUP']}<!|query: str|!>\n")
         return ret + "".join(funcs) if len(funcs) > 0 else ""
     
     def LoadPage(self, txt: str, initPosition: str):
@@ -35,12 +35,12 @@ class AScrollablePage():
         return
 
     def SearchDown(self, query: str) -> bool:
-        loc = self.txt.find(query, self.currentIdx if 0 < self.currentIdx else 0)
+        loc = self.txt.lower().find(query.lower(), self.currentIdx if 0 < self.currentIdx else 0)
         self.currentIdx = (loc - STEP//2) if -1 != loc else self.currentIdx
         return (-1 != loc)
     
     def SearchUp(self, query: str) -> bool:
-        loc = self.txt.rfind(query, 0, (self.currentIdx + 1) if 0 < (self.currentIdx + 1) else 0)
+        loc = self.txt.lower().rfind(query.lower(), 0, (self.currentIdx + 1) if 0 < (self.currentIdx + 1) else 0)
         self.currentIdx = (loc - STEP//2) if -1 != loc else self.currentIdx
         return (-1 != loc)
     
