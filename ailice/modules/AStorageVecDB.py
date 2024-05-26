2d25e6f9192a0dc2fbed03f4c18a638175446b92
Steven Lu <singvision1729@gmail.com>
2024-05-26 21:26:48 +0800

    1. use gpu for default embedding calculation in vecdb.

--- ailice/modules/AStorageVecDB.py
+++ ailice/modules/AStorageVecDB.py
@@ -40,7 +40,7 @@ class AStorageVecDB():
         self.model = Llama(
             model_path=ggufFile,
             embedding=True,
-            #n_gpu_layers=-1, # Uncomment to use GPU acceleration
+            n_gpu_layers=-1, # Uncomment to use GPU acceleration
             # seed=1337, # Uncomment to set a specific seed
             # n_ctx=2048, # Uncomment to increase the context window
         )
